d4fcb90579807a9957072835439051c4e2b8d209
dstoe <stoeckel.david@gmail.com>
2017-11-13 08:31:05 +0100

    Share globals via app

--- server.py
+++ server.py
@@ -8,53 +8,45 @@ from aiohttp import web
 from data import BeancountInterface
 
 
-def get_users_handler(interface):
-    async def call(message, socket):
-        reply = {"state" : "prompt_users", "users" : interface.get_users()}
-        await socket.send_json(reply)
-    return call
+async def get_users_handler(message, request, socket):
+    reply = {
+            "state" : "prompt_users",
+            "users" : request.app["data_interface"].get_users()}
+    await socket.send_json(reply)
 
 
-def get_items_handler(interface):
-    async def call(message, socket):
-        reply = {
-                "state" : "prompt_items",
-                "user" : message["user"],
-                "items" : interface.get_items()}
-        await socket.send_json(reply)
-    return call
+async def get_items_handler(message, request, socket):
+    reply = {
+            "state" : "prompt_items",
+            "user" : message["user"],
+            "items" : request.app["data_interface"].get_items()}
+    await socket.send_json(reply)
 
 
-def select_handler(interface):
-    async def call(message, socket):
-        if message["item"] is not None:
-            interface.add_posting(message["user"], message["item"])
-        await socket.send_json({"state" : "done"})
-    return call
+async def select_handler(message, request, socket):
+    if message["item"] is not None:
+        request.app["data_interface"].add_posting(message["user"], message["item"])
+    await socket.send_json({"state" : "done"})
 
 
-def flush_handler(interface):
-    async def call(message, socket):
-        interface.flush()
-        await socket.send_json({"state" : "done"})
-    return call
+async def flush_handler(message, request, socket):
+    request.app["data_interface"].flush()
+    await socket.send_json({"state" : "done"})
 
 
-def websocket_handler(consumer_dispatch):
-    async def call(request):
-        log = logging.getLogger("websocket_handler::call")
-        socket = web.WebSocketResponse()
-        await socket.prepare(request)
-        async for raw in socket:
-            if raw.type == aiohttp.WSMsgType.TEXT:
-                msg = json.loads(raw.data)
-                assert(msg["state"] in consumer_dispatch)
-                await consumer_dispatch[msg["state"]](msg, socket)
-            elif raw.type == aiohttp.WSMsgType.ERROR:
-                log.error(socket.exception())
-        log.debug("Websocket closed")
-        return socket
-    return call
+async def websocket_handler(request):
+    log = logging.getLogger("websocket_handler::call")
+    socket = web.WebSocketResponse()
+    await socket.prepare(request)
+    async for raw in socket:
+        if raw.type == aiohttp.WSMsgType.TEXT:
+            msg = json.loads(raw.data)
+            assert(msg["state"] in request.app["consumer_dispatch"])
+            await request.app["consumer_dispatch"][msg["state"]](msg, request, socket)
+        elif raw.type == aiohttp.WSMsgType.ERROR:
+            log.error(socket.exception())
+    log.debug("Websocket closed")
+    return socket
 
 
 if __name__ == "__main__":
@@ -66,15 +58,15 @@ if __name__ == "__main__":
     logging.basicConfig(filename=args.logfile, level=logging.DEBUG)
 
     with contextlib.closing(BeancountInterface(args.transactions)) as iface:
-        consumer_dispatch = {
-                "get_users" : get_users_handler(iface),
-                "get_items" : get_items_handler(iface),
-                "select" : select_handler(iface),
-                "flush" : flush_handler(iface),
-                }
-
-        # Start the webserver
+        # Setup webserver
         app = web.Application()
-        app.router.add_get("/ws", websocket_handler(consumer_dispatch))
+        app["data_interface"] = iface
+        app["consumer_dispatch"] = {
+                "get_users" : get_users_handler,
+                "get_items" : get_items_handler,
+                "select" : select_handler,
+                "flush" : flush_handler}
+        app.router.add_get("/ws", websocket_handler)
         app.router.add_static("/", path="www", name="static")
+        # Start the webserver
         web.run_app(app, host="127.0.0.1", port=8000)
