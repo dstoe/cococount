3fc9a07899cce7f7b14c3a4af124d5e876ee4fc2
dstoe <stoeckel.david@gmail.com>
2017-11-13 07:54:20 +0100

    Add http server to websocket server

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 
 ## Getting Started
 
-Start the websocket server:
+Start the http and websocket server:
 ```bash
 python3 server.py data/example.beancount
 ```
-Start the webserver:
-```bash
-cd www && python3 -m http.server 8000
-```
-Open the interface in the browser at http://localhost:8000 . The transactions will be flushed to the file if the websocket server is interrupted, e.g. when stopping with Ctrl+C.
+Open the interface in the browser at http://localhost:8000/index.html . The transactions will be flushed to the file if the websocket server is interrupted, e.g. when stopping with Ctrl+C.
--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import argparse
 import asyncio
-import websockets
 import json
 import logging
 import contextlib
+import aiohttp
+from aiohttp import web
 from data import BeancountInterface
 
 
 def get_users_handler(interface):
     async def call(message, socket):
         reply = {"state" : "prompt_users", "users" : interface.get_users()}
-        await socket.send(json.dumps(reply))
+        await socket.send_json(reply)
     return call
 
 
@@ -20,7 +21,7 @@ def get_items_handler(interface):
                 "state" : "prompt_items",
                 "user" : message["user"],
                 "items" : interface.get_items()}
-        await socket.send(json.dumps(reply))
+        await socket.send_json(reply)
     return call
 
 
@@ -28,24 +29,31 @@ def select_handler(interface):
     async def call(message, socket):
         if message["item"] is not None:
             interface.add_posting(message["user"], message["item"])
-        await socket.send(json.dumps({"state" : "done"}))
+        await socket.send_json({"state" : "done"})
     return call
 
 
 def flush_handler(interface):
     async def call(message, socket):
         interface.flush()
-        await socket.send(json.dumps({"state" : "done"}))
+        await socket.send_json({"state" : "done"})
     return call
 
 
-def handler(consumer_dispatch):
-    async def call(websocket, path):
-        while True:
-            raw = await websocket.recv()
-            msg = json.loads(raw)
-            assert(msg["state"] in consumer_dispatch)
-            await consumer_dispatch[msg["state"]](msg, websocket)
+def websocket_handler(consumer_dispatch):
+    async def call(request):
+        log = logging.getLogger("websocket_handler::call")
+        socket = web.WebSocketResponse()
+        await socket.prepare(request)
+        async for raw in socket:
+            if raw.type == aiohttp.WSMsgType.TEXT:
+                msg = json.loads(raw.data)
+                assert(msg["state"] in consumer_dispatch)
+                await consumer_dispatch[msg["state"]](msg, socket)
+            elif raw.type == aiohttp.WSMsgType.ERROR:
+                log.error(socket.exception())
+        log.debug("Websocket closed")
+        return socket
     return call
 
 
@@ -66,7 +74,7 @@ if __name__ == "__main__":
                 }
 
         # Start the webserver
-        start_server = websockets.serve(handler(consumer_dispatch), "0", 8765)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(start_server)
-        loop.run_forever()
+        app = web.Application()
+        app.router.add_get("/ws", websocket_handler(consumer_dispatch))
+        app.router.add_static("/", path="www", name="static")
+        web.run_app(app, host="127.0.0.1", port=8000)
